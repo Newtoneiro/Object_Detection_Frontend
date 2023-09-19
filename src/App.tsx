4bb2f309ff0f71fb011c476866e6c388493afc5a
Newtoneiro <latosek.bar@gmail.com>
2023-09-19 16:39:34 +0200

    progres wsteczny

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { useEffect } from "react";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import "expo-dev-client";
 
 import AppContextProvider from "./contexts/AppContextProvider";
 import AuthGate from "./components/AuthGate/AuthGate";
 import ErrorPopup from "./components/Utils/ErrorPopup/ErrorPopup";
-import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import LoadingOverlay from "./components/Utils/LoadingOverlay/LoadingOverlay";
 import { Montserrat_500Medium } from "@expo-google-fonts/montserrat";
 import { Poppins_600SemiBold } from "@expo-google-fonts/poppins";
@@ -11,13 +12,16 @@ import { WEB_CLIENT_ID } from "@env";
 import { useFonts } from "expo-font";
 
 const App = () => {
-  GoogleSignin.configure({
-    webClientId: WEB_CLIENT_ID,
-  });
   const [fontsLoaded] = useFonts({
     Montserrat_500Medium,
     Poppins_600SemiBold,
   });
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: WEB_CLIENT_ID,
+    });
+  }, []);
 
   return (
     fontsLoaded && (

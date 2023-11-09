0810145a90792baabd35125345878f08f59f0528
Newtoneiro <latosek.bar@gmail.com>
2023-11-09 09:50:38 +0100

    finished unit test base

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -2,12 +2,6 @@ import React from "react";
 import renderer, { ReactTestRendererJSON } from "react-test-renderer";
 import App from "../src/App";
 
-jest.mock("@react-native-async-storage/async-storage");
-jest.mock("@appandflow/expo-camera-characteristics");
-jest.mock("@react-native-firebase/auth");
-jest.mock("@react-native-google-signin/google-signin");
-jest.mock("@tensorflow/tfjs-react-native");
-
 describe("App component", () => {
   it("has 1 child", () => {
     const tree = renderer.create(<App />).toJSON() as ReactTestRendererJSON;
